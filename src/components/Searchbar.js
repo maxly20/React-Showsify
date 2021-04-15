11201d46652b1ace6af6aaa6ea6801b0a4c94275
maxly20 <maxly2011@gmail.com>
2021-04-15 11:55:31 +1000

    fixing color variables

--- src/components/Searchbar.js
+++ src/components/Searchbar.js
@@ -1,7 +1,26 @@
+import { useState } from 'react';
+
 const Searchbar = () => {
-  return <div className='searchbar'>
-      
-  </div>;
+  const [searchTerm, setSearchTerm] = useState('');
+  const onSearchHandler = e => {
+    e.preventDefault();
+    console.log('searching shows');
+  };
+  return (
+    <div className='searchbar'>
+      <form className='searchbar__form'>
+        <input
+          type='text'
+          placeholder='Search Your Favorite TV Shows'
+          value={searchTerm}
+          onChange={e => setSearchTerm(e.target.value)}
+        />
+        <button className='btn btn-block' onClick={onSearchHandler}>
+          SEARCH
+        </button>
+      </form>
+    </div>
+  );
 };
 
 export default Searchbar;
